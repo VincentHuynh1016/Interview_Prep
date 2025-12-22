28e8c73c5bf7b33e2e0496ee1d8462d13ea4dd05
VincentHuynh1016 <VincentVinhHuynh@gmail.com>
2025-12-22 02:44:32 -0800

    Add simple question bank

--- app/interview/page.tsx
+++ app/interview/page.tsx
@@ -1,8 +1,15 @@
 "use client";
-import {useEffect, useRef} from "react";
+import {useEffect, useRef, useState} from "react";
 
 export default function interviewPage() {
     const videoRef = useRef<HTMLVideoElement>(null);
+    const [questions, setQuestions] = useState<any[]>([]);
+
+    useEffect(() => {
+      fetch("/questions.json")
+        .then((res) => res.json())
+        .then((data) => setQuestions(data.questions));
+    }, []);
 
       useEffect(() => {
         navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
@@ -17,13 +24,17 @@ export default function interviewPage() {
         className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans"
         style={{ backgroundColor: "rgb(43, 48, 59)" }}
       >
-        <div className="flex items-center gap-4 rounded-lg">
+        <div className="flex items-center gap-4">
           <video
             ref={videoRef}
             autoPlay
             className="w-160 h-120 rounded-lg object-cover"
           />
-          <div className="flex bg-black text-white w-160 h-120">Hello </div>
+          <div className="bg-black text-white w-160 h-120 rounded-lg overflow-y-auto p-4">
+            {questions.map((q) => (
+              <p key={q.id}>{q.text}</p>
+            ))}
+          </div>
         </div>
       </div>
     );}
